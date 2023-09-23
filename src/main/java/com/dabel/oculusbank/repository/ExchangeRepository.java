dea2bd8edba89d294bcf60fd9ddbb314efb76767
abdelnasserben <benaliabdelnasserpro@gmail.com>
2023-09-23 20:25:23 +0300

    no important modification of the last commit. it's just optimze import and only the exchangerepository has been optimazed

--- src/main/java/com/dabel/oculusbank/repository/ExchangeRepository.java
+++ src/main/java/com/dabel/oculusbank/repository/ExchangeRepository.java
@@ -3,7 +3,6 @@ package com.dabel.oculusbank.repository;
 import com.dabel.oculusbank.model.Exchange;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 public interface ExchangeRepository extends JpaRepository<Exchange, Integer> {
